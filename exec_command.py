81305146d7b60479af5750853fcc55c34599700a
Melissa Li <melissali@localhost.localdomain>
2021-06-14 15:31:32 -0400

    Initial commit

---
+++ exec_command.py
@@ -0,0 +1,35 @@
+import paramiko
+from contextlib import contextmanager
+
+@contextmanager
+def connection(addr):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(addr, username='melissali', key_filename='/home/melissali/.ssh/id_rsa')
+    yield client
+
+def run():
+    with connection('192.168.2.249') as conn:
+        cmd = 'python3'
+        stdin = "print('hello')"
+        execute_command(conn, cmd, stdin)
+
+def execute_command(conn, cmd, stdin = ""):
+    _stdin, _stdout, _stderr = conn.exec_command(cmd)
+    if stdin:
+        _stdin.write(stdin)
+        _stdin.flush()
+        _stdin.channel.shutdown_write()
+    out = ''
+    for line in _stdout:
+        out += line
+    # print(type(out))
+    print(out)
+    err = ''
+    for line in _stderr:
+        err += line
+    print(err)
+    code = _stdout.channel.recv_exit_status()
+    print(code)
+
+run()
